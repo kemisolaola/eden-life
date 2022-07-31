5e56cd8c2211f6e5ac342fd6b7d25fc67bed6d81
Oluwakemi <Oluwakemi.olatunji@sterling.ng>
2022-07-31 18:55:18 +0100

    final commit

--- src/components/DogCard.vue
+++ src/components/DogCard.vue
@@ -1,27 +1,4 @@
 <template>
-<v-container fluid>
-  <v-row justify="center">
-    <v-col cols="12" md="3">
-      <select @change="fetchDogsByBreed" v-model="selectedDog" class="select-field pl-2" >
-      <option value="" default> Select Dog</option>
-        <option v-for="(dogs, name, index) in breedList" :key="index" :value="name">{{name.toUpperCase()}}</option>
-      </select>
-    </v-col>
-  </v-row>
-  <v-row class="px-sm-10">
-    <v-col cols="12" class="px-5">
-      <v-card class="pa-5" elevation="1">
-        <v-row justify="space-between">
-          <v-col>
-       <p class="text-h5 text-sm-h4 text-left"> {{selectedDog.toUpperCase()}} </p>
-          </v-col>
-           <v-col>
-       <p class="text-h5  text-sm-h4 text-right"> {{getDogImages.length}} <span class="text-h5">results</span></p>
-          </v-col>
-        </v-row>
-      </v-card>
-    </v-col>
-    </v-row>
     <v-row>
     <v-col cols="3" sm="12" class="mt-16 mx-auto" v-if="isLoading">
        <p class="text-center mt-sm-10"><v-progress-circular indeterminate></v-progress-circular></p>
@@ -41,7 +18,6 @@
     </v-lazy>
     </v-col>
   </v-row>
-</v-container>
 </template>
 
 <script>
@@ -84,13 +60,3 @@ export default {
   }
 }
 </script>
-
-<style>
-.select-field{
-    background-color: #fff;
-    border: 1px solid #aaa;
-    border-radius: 4px;
-    width: 100%;
-    height: 50px;
-}
-</style>
