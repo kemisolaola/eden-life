973a5aa82a4e6d3b6a348060125a7f42d2f81506
Oluwakemi <Oluwakemi.olatunji@sterling.ng>
2022-07-31 10:33:05 +0100

    setup home page and consume an api in store

--- src/store/index.js
+++ src/store/index.js
@@ -5,12 +5,42 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    dogs: null,
+    dogImages: null
   },
   getters: {
+    getDogs: (state) => state.dogs,
+    getDogImages: (state) => state.dogImages
   },
   mutations: {
+    SET_DOGS: (state, dogs) => {
+      state.dogs = dogs
+    },
+    SET_DOGIMAGES: (state, dogImages) => {
+      state.dogImages = dogImages
+    }
   },
   actions: {
+    async fetchDogs ({ commit }) {
+      const url = 'https://dog.ceo/api/breeds/list/all'
+      const response = await fetch(url, {
+        method: 'GET'
+      })
+      console.log(response)
+      const res = await response.json()
+      console.log(res)
+      commit('SET_DOGS', res.message)
+    },
+    async fetchDogsImages ({ commit }) {
+      const url = 'https://dog.ceo/api/breed/hound/images'
+      const response = await fetch(url, {
+        method: 'GET'
+      })
+      console.log(response)
+      const res = await response.json()
+      console.log(res)
+      commit('SET_DOGIMAGES', res.message)
+    }
   },
   modules: {
   }
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,57 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+<v-container fluid>
+  <v-row justify="end">
+    <v-col cols="6">
+      <select v-model="selectedDog">
+      <option value="" default> Select Dog</option>
+        <option v-for="(dogs, name, index) in getDogs" :key="index" :value="name">{{name.toUpperCase()}}</option>
+      </select>
+    </v-col>
+  </v-row>
+  <v-row>
+    <v-col cols="12">
+      <v-card height="100px" elevation="1">
+        {{selectedDog.toUpperCase()}}
+
+        <p class="text-right"> {{getDogImages.length}} </p>
+      </v-card>
+    </v-col>
+    <v-col cols="12" sm="4" v-for="(dogPic, index) in hundredDogs" :key="index">
+    <v-card outlined width="350" height="350" class="pa-4">
+      <v-img width="100%" height="100%" :src="dogPic"></v-img>
+    </v-card>
+    </v-col>
+  </v-row>
+</v-container>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import { mapActions, mapGetters } from 'vuex'
 export default {
   name: 'HomeView',
-  components: {
-    HelloWorld
+  data () {
+    return {
+      selectedDog: 'None Selected'
+    }
+  },
+  computed: {
+    ...mapGetters([
+      'getDogs',
+      'getDogImages'
+    ]),
+    hundredDogs () {
+      return this.getDogImages.slice(300, 400)
+    }
+  },
+  methods: {
+    ...mapActions([
+      'fetchDogs',
+      'fetchDogsImages'
+    ])
+  },
+  mounted () {
+    this.fetchDogs()
+    this.fetchDogsImages()
   }
 }
 </script>
